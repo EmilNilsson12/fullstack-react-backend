310db5f7f3833935e9e8fc39f4aad1e3de0baac1
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-07-17 22:06:28 +0200

    Add upvoting route and install nodemon

--- src/server.js
+++ src/server.js
@@ -1,9 +1,33 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const articlesInfo = {
+	'learn-react': {
+		upvotes: 0,
+	},
+	'learn-node': {
+		upvotes: 0,
+	},
+	'my-thoughts-on-resumes': {
+		upvotes: 0,
+	},
+};
+
 const app = express();
 
 app.use(bodyParser.json());
+
+app.post('/api/articles/:name/upvote', (req, res) => {
+	const articleName = req.params.name;
+
+	articlesInfo[articleName].upvotes += 1;
+
+	return res
+		.status(200)
+		.send(
+			`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`
+		);
+});
 
 app.get('/hello', (req, res) => {
 	return res.send('Hello');
