da7ab4122e876de057304c85b501c304d2fb5315
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-07-18 13:35:10 +0200

    Remove server-only data of articles

--- src/server.js
+++ src/server.js
@@ -2,21 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
 
-const articlesInfo = {
-	'learn-react': {
-		upvotes: 0,
-		comments: [],
-	},
-	'learn-node': {
-		upvotes: 0,
-		comments: [],
-	},
-	'my-thoughts-on-resumes': {
-		upvotes: 0,
-		comments: [],
-	},
-};
-
 const app = express();
 
 app.use(bodyParser.json());
